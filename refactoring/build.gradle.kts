26c84c134ac8b427aa59849df398c782985a6501
Daniil Monakhov <dmonakhov@devexperts.com>
2022-11-06 22:25:38 +0300

    web server added

--- refactoring/build.gradle.kts
+++ refactoring/build.gradle.kts
@@ -14,7 +14,6 @@ dependencies {
     implementation("org.eclipse.jetty:jetty-server:11.0.12")
     implementation("org.postgresql:postgresql:42.3.7")
     implementation("com.zaxxer:HikariCP:5.0.1")
-//    implementation("javax.servlet:javax.servlet-api:3.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("org.testcontainers:postgresql:1.17.3")
     testImplementation("org.testcontainers:junit-jupiter:1.17.5")
