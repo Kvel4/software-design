5e5a4ad92395538e72136cb06d2cd99f542faff6
Daniil Monakhov <dmonakhov@devexperts.com>
2022-11-07 04:04:42 +0300

    super cool repository

--- refactoring/src/main/java/ru/itmo/repositoty/JPARepository.java
+++ refactoring/src/main/java/ru/itmo/repositoty/JPARepository.java
@@ -27,96 +27,91 @@ public class JPARepository {
     }
 
     public void insertProduct(Product product) throws SQLException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                 "insert into Product (Name, Price) values (?, ?);"
-             )
-        ) {
-            statement.setString(1, product.name());
-            statement.setLong(2, product.price());
+       executeStatement(statement -> {
+           String sql = String.format(
+               "insert into Product (Name, Price) values ('%s', %s);",
+               product.name(),
+               product.price()
+           );
 
-            statement.executeUpdate();
-        }
+           return statement.executeUpdate(sql);
+       });
     }
 
     public List<Product> findAllProducts() throws SQLException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement("select * from Product;")
-        ) {
-            return fetchListProductFromSql(statement);
-        }
+        return executeStatement(statement -> {
+            ResultSet resultSet = statement.executeQuery("select * from Product;");
+            return fetchListProductFromSql(resultSet);
+        });
     }
 
     public Product maxProductByPrice() throws SQLException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                 "select * from Product order by price desc limit 1;"
-             )
-        ) {
-            List<Product> productList = fetchListProductFromSql(statement);
+        return executeStatement(statement -> {
+            ResultSet resultSet = statement.executeQuery("select * from Product order by price desc limit 1;");
+            List<Product> productList = fetchListProductFromSql(resultSet);
 
             assert productList.size() == 1;
 
             return productList.get(0);
-        }
+        });
     }
 
     public Product minProductByPrice() throws SQLException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(
-                 "select * from Product order by price limit 1;"
-             )
-        ) {
-            List<Product> productList = fetchListProductFromSql(statement);
+        return executeStatement(statement -> {
+            ResultSet resultSet = statement.executeQuery( "select * from Product order by price limit 1;");
+            List<Product> productList = fetchListProductFromSql(resultSet);
 
             assert productList.size() == 1;
 
             return productList.get(0);
-        }
+        });
     }
 
     public int countProducts() throws SQLException {
-        try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement();
-        ) {
+        return executeStatement(statement -> {
             ResultSet resultSet = statement.executeQuery("select count(*) from Product;");
             resultSet.next();
 
             return resultSet.getInt(1);
-        }
+        });
     }
 
     public long summaryPrice() throws SQLException {
-        try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement();
-        ) {
+        return executeStatement(statement -> {
             ResultSet resultSet = statement.executeQuery("select sum(price) from Product;");
             resultSet.next();
 
             return resultSet.getLong(1);
-        }
+        });
     }
 
     public void clear() throws SQLException {
+        executeStatement(statement -> statement.executeUpdate("delete from Product;"));
+    }
+
+    private <T> T executeStatement(SqlFunction<T> function) throws SQLException {
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement();
         ) {
-            statement.executeUpdate("delete from Product;");
+            return function.apply(statement);
         }
     }
 
-    private List<Product> fetchListProductFromSql(PreparedStatement statement) throws SQLException {
-        try (ResultSet resultSet = statement.executeQuery()) {
-            ArrayList<Product> products = new ArrayList<>();
-            while (resultSet.next()) {
-                products.add(
-                    new Product(
-                        resultSet.getString("name"),
-                        resultSet.getInt("price")
-                    )
-                );
-            }
-            return products;
+    private List<Product> fetchListProductFromSql(ResultSet resultSet) throws SQLException {
+        ArrayList<Product> products = new ArrayList<>();
+        while (resultSet.next()) {
+            products.add(
+                new Product(
+                    resultSet.getString("name"),
+                    resultSet.getInt("price")
+                )
+            );
         }
+        return products;
+    }
+
+    @FunctionalInterface
+    interface SqlFunction<T> {
+        T apply(Statement statement) throws SQLException;
     }
 }
